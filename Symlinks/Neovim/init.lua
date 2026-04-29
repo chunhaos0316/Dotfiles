6f5e97c152b41844f3c70b6dd48b0abbd38c28cb
chunhaos0316 <chunhaos0316@gmail.com>
2026-04-29 21:20:05 +0800

    Change neovim scheme to tokyonight and highlight settings

--- Symlinks/Neovim/init.lua
+++ Symlinks/Neovim/init.lua
@@ -49,11 +49,67 @@ require("lazy").setup({
   -- Syntax and LSP
   "nvim-treesitter/nvim-treesitter",
   "neovim/nvim-lspconfig",
+
+  -- Tokyonight Colorscheme
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      style = "night", -- storm, night, moon, day
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+      on_colors = function(colors)
+        colors.bg = "#000000"
+        colors.bg_dark = "#000000"
+        colors.bg_float = "#000000"
+        colors.border = "#FFFFFF"
+        colors.fg = "#DCDCDC"
+      end,
+
+      on_highlights = function(hl, c)
+        hl.Normal = { fg = "#C0C0C0", bg = "#000000" }
+        hl.Comment = { fg = "#87CEFA", italic = false }
+
+        hl.Constant = { fg = "#7B68EE" }
+        hl.String = { fg = "#3CB371" }
+
+        hl.Statement = { fg = "#F0E68C" }
+        hl.Conditional = { fg = "#F0E68C" }
+        hl.Repeat = { fg = "#F0E68C" }
+        hl.Operator = { fg = "#DCDCDC" }
+
+        hl.Type = { fg = "#00BFFF" }      -- (int, char)
+        hl.PreProc = { fg = "#FFD700" }   -- (#include)
+
+        hl.Special = { fg = "#DDA0DD" }   -- (\n)
+
+        hl.Identifier = { fg = "#00D7FF" }
+        hl.Function = { fg = "#00D7FF" }
+
+        -- *** 優化 Treesitter (現代高亮) 以符合 Console 風格 ***
+        -- 很多現代高亮會給結構體成員顏色，但截圖中沒有
+        hl["@property"] = { fg = "#DCDCDC" }
+        hl["@parameter"] = { fg = "#DCDCDC" }
+
+        hl.LineNr = { fg = "#48D1CC", bg = "#000000" }
+
+        hl.Visual = { bg = "#555555" }
+      end,
+    },
+    config = function(_, opts)
+      require("tokyonight").setup(opts)
+      vim.cmd([[colorscheme tokyonight]])
+    end,
+  },
 })
 
 -- 5. Basic Neovim settings
 vim.opt.number = true             -- Show line numbers
-vim.opt.relativenumber = true     -- Show relative line numbers for easier jumping
+vim.opt.relativenumber = false    -- Show relative line numbers for easier jumping
 vim.opt.shiftwidth = 4            -- Size of an indent
 vim.opt.expandtab = true          -- Use spaces instead of tabs
 vim.opt.smartindent = true        -- Insert indents automatically
